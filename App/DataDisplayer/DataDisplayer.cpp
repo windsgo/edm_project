794de373975458aec4c0bb9155120b644919be9b
windsgo <18221102427@163.com>
2024-04-02 17:02:44 +0800

    temp save 20240402 1702

--- App/DataDisplayer/DataDisplayer.cpp
+++ App/DataDisplayer/DataDisplayer.cpp
@@ -1,14 +1,267 @@
 #include "DataDisplayer.h"
+#include "qwt_legend.h"
+#include "qwt_plot_curve.h"
 #include "ui_DataDisplayer.h"
+#include <cassert>
+#include <qcombobox.h>
+#include <qglobal.h>
+#include <qnamespace.h>
+#include <qpushbutton.h>
+#include <qslider.h>
+#include <qvector.h>
 
-DataDisplayer::DataDisplayer(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::DataDisplayer)
-{
+#include "qwt_plot_grid.h"
+
+namespace edm {
+namespace app {
+
+DataDisplayer::DataDisplayer(QWidget *parent)
+    : QWidget(parent), ui(new Ui::DataDisplayer) {
     ui->setupUi(this);
+
+    // init title
+    ui->qwtPlot->setAxisTitle(QwtPlot::xBottom, "x");
+    ui->qwtPlot->setAxisTitle(QwtPlot::yLeft, "y");
+    ui->qwtPlot->setAxisAutoScale(QwtPlot::xBottom, true);
+    ui->qwtPlot->setStyleSheet("color: white;");
+
+    // init legend
+    legend_ = new QwtLegend();
+    ui->qwtPlot->insertLegend(legend_);
+
+    // init grid
+    QwtPlotGrid* grid = new QwtPlotGrid();
+    grid->setPen(Qt::gray, 0.0, Qt::DotLine);
+    grid->enableX(true);
+    grid->enableY(true);
+    grid->attach(ui->qwtPlot);
+
+    _init_buttons();
+    _update_settings_from_ui();
+    _replot();
 }
 
-DataDisplayer::~DataDisplayer()
-{
-    delete ui;
+int DataDisplayer::add_data_item(const DisplayedDataDesc &data_desc) {
+    int next_index = datas_.size();
+
+    _DataInfo data;
+    data.data_desc = data_desc;
+
+    // init vec
+    if (data.data_desc.data_max_points <= 0) {
+        data.data_vec.resize(x_points_);
+    } else {
+        if (data.data_desc.data_max_points > x_points_) {
+            data.data_desc.data_max_points = x_points_;
+        }
+
+        data.data_vec.resize(data.data_desc.data_max_points);
+    }
+    data.data_vec.fill(0.0);
+
+    if (data.data_desc.y_scale_percent <= 0 ||
+        data.data_desc.y_scale_percent >
+            ui->verticalSlider_scale_percent->maximum()) {
+        data.data_desc.y_scale_percent = 100;
+    }
+    if (data.data_desc.y_zero_offset <
+            ui->verticalSlider_zero_offset->minimum() ||
+        data.data_desc.y_zero_offset >
+            ui->verticalSlider_zero_offset->maximum()) {
+        data.data_desc.y_zero_offset = 0;
+    }
+
+    ui->comboBox_item_select->addItem(data.data_desc.data_name);
+
+    // init ui data if first (scale zero visible)
+    _set_ui_data_desc_from_member_data_desc();
+
+    // init curve
+    data.curve = new QwtPlotCurve(data.data_desc.data_name);
+    if (data.data_desc.visible) {
+        data.curve->attach(ui->qwtPlot);
+        data.curve->setPen(data.data_desc.preferred_color);
+    }
+
+    // set curve init samples
+    _update_curve_from_vec(data);
+
+    datas_.push_back(data);
+    Q_ASSERT(ui->comboBox_item_select->maxCount() == datas_.size());
+    return next_index;
 }
+
+bool DataDisplayer::push_data(int index, double v) {
+    if (!enable_) {
+        return false;
+    }
+
+    if (index < 0 || index >= datas_.size()) {
+        return false;
+    }
+
+    auto& data = datas_[index];
+
+    data.data_vec.pop_front();
+    data.data_vec.push_back(v);
+
+    return true;
+}
+
+void DataDisplayer::update_display() {
+    if (!this->isVisible()) {
+        return;
+    }
+
+    _replot();
+}
+
+DataDisplayer::~DataDisplayer() { delete ui; }
+
+void DataDisplayer::_init_buttons() {
+    connect(ui->pb_update, &QPushButton::clicked, this, [this]() {
+        _update_settings_from_ui();
+        _replot();
+    });
+
+    connect(ui->pb_item_visible, &QPushButton::clicked, this, [this](bool checked) {
+        auto index = ui->comboBox_item_select->currentIndex();
+        if (index < 0) {
+            return;
+        }
+
+        auto& data = datas_[index];
+
+        if (!data.data_desc.visible && checked) {
+            data.curve->attach(ui->qwtPlot);
+        } else if (data.data_desc.visible && !checked) {
+            data.curve->detach();
+        }
+
+        data.data_desc.visible = checked;
+
+        _replot();
+    });
+
+    connect(ui->comboBox_item_select, QOverload<int>::of(&QComboBox::currentIndexChanged), this, [this](int index) {
+        if (index < 0) return;
+
+        _set_ui_data_desc_from_member_data_desc();
+    }); 
+
+    connect(ui->verticalSlider_zero_offset, &QSlider::valueChanged, this, [this](int value) {
+        auto index = ui->comboBox_item_select->currentIndex();
+        if (index < 0) {
+            return;
+        }
+        auto& data = datas_[index];
+
+        data.data_desc.y_zero_offset = value;
+    });
+
+    connect(ui->verticalSlider_scale_percent, &QSlider::valueChanged, this, [this](int value) {
+        auto index = ui->comboBox_item_select->currentIndex();
+        if (index < 0) {
+            return;
+        }
+        auto& data = datas_[index];
+
+        data.data_desc.y_scale_percent = value;
+    });
+}
+
+void DataDisplayer::_replot() {
+    if (!enable_) {
+        return;
+    }
+
+    for (auto& data : datas_) {
+        if (!data.data_desc.visible) {
+            continue;
+        }
+
+        _update_curve_from_vec(data);
+    }
+
+    ui->qwtPlot->replot();
+}
+
+void DataDisplayer::_update_settings_from_ui() {
+    int old_x_points = x_points_;
+
+    x_points_ = ui->sb_points->value();
+    if (x_points_ < 1) {
+        x_points_ = 1;
+        ui->sb_points->setValue(x_points_);
+    }
+
+    if (old_x_points != x_points_) {
+        _resize_plotted_vector(x_vec_, x_points_);
+        ui->qwtPlot->setAxisScale(QwtPlot::xBottom, 0, x_points_ - 1);
+
+        for (auto &data : datas_) {
+            if (data.data_desc.data_max_points <= 0 ||
+                data.data_desc.data_max_points > x_points_) {
+                _resize_plotted_vector(data.data_vec, x_points_);
+            } else {
+                _resize_plotted_vector(data.data_vec,
+                                       data.data_desc.data_max_points);
+            }
+        }
+    }
+
+    // deal with ymin ymax
+    if (ui->sb_ymin->value() >= ui->sb_ymax->value()) {
+        // reset min and max
+        ui->sb_ymin->setValue(y_min_);
+        ui->sb_ymax->setValue(y_max_);
+    }
+
+    // get min and max from ui
+    y_min_ = ui->sb_ymin->value();
+    y_max_ = ui->sb_ymax->value();
+
+    ui->qwtPlot->setAxisScale(QwtPlot::yLeft, y_min_, y_max_);
+}
+
+void DataDisplayer::_resize_plotted_vector(QVector<double> &vec,
+                                           int new_length) {
+    if (vec.size() == new_length || new_length <= 0) {
+        return;
+    }
+
+    if (vec.size() > new_length) {
+        // 删除vec靠前的一部分, 以达到new_length长度
+        vec = vec.mid(vec.size() - new_length);
+    } else {
+        // vec前面增加一部分0, 以达到new_length长度
+        QVector<double> zero_vec;
+        zero_vec.resize(new_length - vec.size());
+        zero_vec.fill(0);
+
+        vec = zero_vec + vec;
+    }
+}
+
+void DataDisplayer::_set_ui_data_desc_from_member_data_desc() {
+    auto index = ui->comboBox_item_select->currentIndex();
+    if (index < 0) return;
+
+    const auto& data = datas_[index];
+
+    ui->verticalSlider_zero_offset->setValue(data.data_desc.y_zero_offset);
+    ui->verticalSlider_scale_percent->setValue(data.data_desc.y_scale_percent);
+
+    ui->pb_item_visible->setChecked(data.data_desc.visible);
+}
+
+void DataDisplayer::_update_curve_from_vec(DataDisplayer::_DataInfo& data) {
+    if (data.data_desc.data_max_points <= 0) {
+        data.curve->setSamples(x_vec_, data.data_vec);  
+    } else {
+        data.curve->setSamples(x_vec_.mid(x_points_ - data.data_desc.data_max_points), data.data_vec);
+    }
+}
+
+} // namespace app
+} // namespace edm
--- App/DataDisplayer/DataDisplayer.h
+++ App/DataDisplayer/DataDisplayer.h
@@ -1,11 +1,33 @@
-#ifndef DATADISPLAYER_H
-#define DATADISPLAYER_H
+#pragma once
 
 #include <QWidget>
+#include <qcolor.h>
+#include <qmessagebox.h>
+#include <qnamespace.h>
+#include <qvector.h>
+#include <unordered_map>
+
+#include <qwt_plot_curve.h>
+#include <qwt_legend.h>
 
 namespace Ui {
 class DataDisplayer;
 }
+
+namespace edm { 
+namespace app {
+
+struct DisplayedDataDesc {
+    QString data_name;
+    QString data_detail_desc;
+    int data_max_points {-1}; // 设置为-1(<0的数), 跟随窗口设置点数
+    QColor preferred_color {QColor(Qt::green)};
+
+    int y_zero_offset{0}; // 显示时的y轴零点偏置
+    int y_scale_percent{100}; // 显示时, 实际的坐标值为 (y + y_zero_offset) * y_scale / 100 * 100%
+
+    bool visible{true};
+};
 
 class DataDisplayer : public QWidget
 {
@@ -15,8 +37,54 @@ public:
     explicit DataDisplayer(QWidget *parent = nullptr);
     ~DataDisplayer();
 
+    // 添加一个数据项目, 返回注册的编号(用于push数据时传入编号).
+    // 如果失败, 返回-1 (<0)
+    int add_data_item(const DisplayedDataDesc& data_desc);
+    
+    // 压入对应编号的一个数据
+    bool push_data(int index, double v);
+
+    // 刷新显示
+    void update_display();
+
+private:
+    void _init_buttons();
+
+    void _clear(); // TODO
+    void _replot();
+
+    void _update_settings_from_ui();
+    void _resize_plotted_vector(QVector<double>& vec, int new_length);
+
+    void _set_ui_data_desc_from_member_data_desc();
+
+
 private:
     Ui::DataDisplayer *ui;
+
+    // canvas show scale:
+    int x_points_;
+    double y_min_;
+    double y_max_;
+
+    bool enable_ {true}; // TODO
+    
+private: // member datas
+
+    QVector<double> x_vec_;
+
+    struct _DataInfo {
+        struct DisplayedDataDesc data_desc;
+        QVector<double> data_vec;
+        QwtPlotCurve* curve;
+    };
+
+    void _update_curve_from_vec(_DataInfo& data);
+
+    std::vector<_DataInfo> datas_;
+
+    QwtLegend* legend_;
 };
 
-#endif // DATADISPLAYER_H
+} // namespace app
+} // namespace edm
