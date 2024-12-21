b5c327a9396a95bde3e16cc66ee39e4e2ae10f07
windsgo <18221102427@163.com>
2024-12-21 21:35:44 +0800

    test

---
+++ gcode/test_mill.py
@@ -0,0 +1,39 @@
+i = RS274Interpreter()
+
+i.g55()
+i.f(1000)
+i.g91()
+
+i.f(1000).g90().g00(x=0, y=0, z=0)
+
+# Touch Once
+i.f(100).g91().g00(a=-100, touch=True)
+i.g04(1)
+i.f(500).g91().g00(a=1, m05=True)
+i.g04(1)
+
+# Touch Twice
+i.f(50).g91().g00(a=-5, touch=True)
+i.g04(1)
+# set zero
+i.coord_set_a_zero()
+i.g04(1)
+
+# up
+i.f(500).g91().g00(a=1, m05=True)
+i.g04(1)
+
+# drill
+ps = Points()
+ps.g90()
+ps.push(a=-1) # drill
+
+# mill
+ps.g91()
+ps.push(x=3)
+ps.push(y=3)
+ps.push(x=3, y=3)
+
+i.g01_group(ps)
+
+i.m02()
