bd844d8f807654a234c5b58351f6b40705f7076d
windsgo <18221102427@163.com>
2025-08-14 21:20:08 +0800

    add g57,58,59

--- Src/Interpreter/rs274pyInterpreter/pymodule/rs274.py
+++ Src/Interpreter/rs274pyInterpreter/pymodule/rs274.py
@@ -347,6 +347,12 @@ class RS274Interpreter(object):
         return self._command_set_coordinate_index(55)
     def g56(self) -> RS274Interpreter:
         return self._command_set_coordinate_index(56)
+    def g57(self) -> RS274Interpreter:
+        return self._command_set_coordinate_index(57)
+    def g58(self) -> RS274Interpreter:
+        return self._command_set_coordinate_index(58)
+    def g59(self) -> RS274Interpreter:
+        return self._command_set_coordinate_index(59)
     def set_coord(self, index: int) -> RS274Interpreter:
         return self._command_set_coordinate_index(index)
     # TODO 其他坐标系指定
